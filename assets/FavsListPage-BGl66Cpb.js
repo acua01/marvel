9dab736a4f23889bcb0e9c1b8976f5a1b78b0967
Alejandro Acuaviva <ale.ap98@hotmail.com>
2024-06-24 21:52:14 +0200

    Updates

---
+++ assets/FavsListPage-BGl66Cpb.js
@@ -0,0 +1,1 @@
+import{r,F as a,j as o}from"./index-T6sdR-Tq.js";import{C as e}from"./CharactersListLayout-XmaBsFQi.js";import{u as n}from"./useScrollUp-CkdqHaAU.js";const c=()=>{const s=r.useContext(a),{favs:t}=s;return n(),{favs:t}},i=()=>{const{favs:s}=c();return o.jsx(e,{characters:s})};export{i as default};
---
+++ assets/NotFoundPage-B9_xJvPB.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-T6sdR-Tq.js";import{E as s}from"./ErrorMessage-B9SAdjDY.js";const t=()=>o.jsx(s,{message:"Page not found"});export{t as default};
