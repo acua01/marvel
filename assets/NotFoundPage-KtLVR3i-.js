247127b0a65705610e916d236694be3a6e1058a5
Alejandro Acuaviva <ale.ap98@hotmail.com>
2024-06-24 22:07:29 +0200

    Updates

---
+++ assets/NotFoundPage-KtLVR3i-.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DgbZ5lUp.js";import{E as s}from"./ErrorMessage-CiBgzfx5.js";const t=()=>o.jsx(s,{message:"Page not found"});export{t as default};
--- assets/NotFoundPage-_yqL7wXc.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Bc71I7BR.js";import{E as s}from"./ErrorMessage-DFY2u3Lb.js";const t=()=>o.jsx(s,{message:"Page not found"});export{t as default};
